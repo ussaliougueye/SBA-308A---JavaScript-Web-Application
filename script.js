05949f2a3845eac6492188c2ea042f5ab52ba6d6
ussaliougueye <sn.saliougueye@gmail.com>
2025-06-06 17:20:30 -0400

    Add files via upload
    updates after adding external file that are going to be imported in the scripts.js

--- script.js
+++ script.js
@@ -1,112 +1,12 @@
-let cityList = [
-  "New York",
-  "Los Angeles",
-  "Chicago",
-  "Houston",
-  "Phoenix",
-  "Toronto",
-  "Vancouver",
-  "Montreal",
-  "Calgary",
-  "Ottawa",
-  "Mexico City",
-  "Guadalajara",
-  "Monterrey",
-  "Tijuana",
-  "Cancún",
-  "São Paulo",
-  "Rio de Janeiro",
-  "Buenos Aires",
-  "Lima",
-  "Bogotá",
-  "Santiago",
-  "Caracas",
-  "Quito",
-  "La Paz",
-  "Montevideo",
-  "London",
-  "Paris",
-  "Berlin",
-  "Madrid",
-  "Rome",
-  "Amsterdam",
-  "Brussels",
-  "Vienna",
-  "Copenhagen",
-  "Stockholm",
-  "Oslo",
-  "Dublin",
-  "Lisbon",
-  "Warsaw",
-  "Prague",
-  "Zurich",
-  "Athens",
-  "Budapest",
-  "Milan",
-  "Munich",
-  "Tokyo",
-  "Osaka",
-  "Seoul",
-  "Beijing",
-  "Shanghai",
-  "Bangkok",
-  "Singapore",
-  "Kuala Lumpur",
-  "Jakarta",
-  "Manila",
-  "Mumbai",
-  "Delhi",
-  "Bangalore",
-  "Chennai",
-  "Islamabad",
-  "Karachi",
-  "Kathmandu",
-  "Hanoi",
-  "Ho Chi Minh City",
-  "Dhaka",
-  "Dubai",
-  "Abu Dhabi",
-  "Riyadh",
-  "Jeddah",
-  "Tehran",
-  "Istanbul",
-  "Jerusalem",
-  "Amman",
-  "Baghdad",
-  "Doha",
-  "Cairo",
-  "Lagos",
-  "Nairobi",
-  "Johannesburg",
-  "Cape Town",
-  "Casablanca",
-  "Accra",
-  "Addis Ababa",
-  "Algiers",
-  "Tunis",
-  "Sydney",
-  "Melbourne",
-  "Brisbane",
-  "Perth",
-  "Adelaide",
-  "Auckland",
-  "Wellington",
-  "Christchurch",
-  "Suva",
-  "Port Moresby",
-];
+import { populateCityList } from './functions.js';
+
+window.onload = function() {
+  populateCityList();
+};
 const lat = 37.7749;
 const lon = -122.4194;
 const apiKey = "6fa07ab67bd148ac92c173725250206";
 let pickedCity = "";
-window.onload = () => {
-  cityList.forEach((city) => {
-    const option = document.createElement("option");
-    option.value =city;
-    option.textContent = city;
-    document.getElementById("cityy").appendChild(option);
-  });
-};
 
 document.getElementById("cityy").addEventListener("change", (event) => {
   pickedCity = event.target.value;
